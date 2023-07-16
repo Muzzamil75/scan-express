057b430ebb7be7e948c33db1168c54f451c0023c
M Muzzammil ijaz <muzzammilijaz@Muzzammil-Ijaz.local>
2023-07-16 17:09:00 +0500

    Added react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/main.scss';
 import App from './routes/Routes';
 import reportWebVitals from './reportWebVitals';
 
