adbd03d64a4f036d0980ba03014844b9aa0241a5
M Muzzammil ijaz <muzzammilijaz@Muzzammil-Ijaz.local>
2023-07-18 15:02:52 +0500

    intial one

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './css/main.scss';
 import App from './routes/Routes';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
